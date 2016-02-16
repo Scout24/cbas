0a87edf03b7f66f292072bbb3241440cbcf7a2c4
Valentin Haenel <valentin@haenel.co>
2016-02-16 11:02:44 +0100

    simplify getting access token

--- src/main/python/cbas/auth_server.py
+++ src/main/python/cbas/auth_server.py
@@ -1,12 +1,12 @@
 import requests
 
 
-def obtain_access_token(auth_url, client_secret, username, password):
+def obtain_access_token(config, password):
     auth_request_data = {'client_id': 'jumpauth',
-                         'client_secret': client_secret,
-                         'username': username,
+                         'client_secret': config.client_secret,
+                         'username': config.username,
                          'password': password,
                          'grant_type': 'password'}
-    auth_response = requests.post(auth_url, auth_request_data)
+    auth_response = requests.post(config.auth_url, auth_request_data)
     auth_response.raise_for_status()
     return auth_response.json()['access_token']
